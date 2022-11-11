94ccd39ef030427bcd792782a1c9f94b0cf92d31
gomin0 <gomin0981113@naver.com>
2022-11-11 13:53:39 +0900

    포스트 목록 페이지와 포스트 상세 페이지에 태그 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class TestView(TestCase):
     def setUp(self):
@@ -12,12 +12,17 @@ class TestView(TestCase):
         self.category_soccer = Category.objects.create(name='soccer', slug='soccer')
         self.category_star = Category.objects.create(name='star', slug='star')
 
+        self.tag_ggmu = Tag.objects.create(name='맨유', slug='맨유')
+        self.tag_love = Tag.objects.create(name='좋아요', slug='좋아요')
+        self.tag_hello = Tag.objects.create(name='hello', slug='hello')
+
         self.post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
             category=self.category_soccer,
             author=self.user_gomin0
         )
+        self.post_001.tags.add(self.tag_hello)
 
         self.post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
@@ -31,6 +36,8 @@ class TestView(TestCase):
             content='Hello World. We are the world.',
             author=self.user_gomin0
         )
+        self.post_003.tags.add(self.tag_ggmu)
+        self.post_003.tags.add(self.tag_love)
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -73,14 +80,26 @@ class TestView(TestCase):
         post_001_card = main_area.find('div', id='post-1')  # id가 post-1인 div를 찾아서, 그 안에
         self.assertIn(self.post_001.title, post_001_card.text)  # title이 있는지
         self.assertIn(self.post_001.category.name, post_001_card.text)  # category가 있는지
+        self.assertIn(self.post_001.author.username.upper(), post_001_card.text)
+        self.assertIn(self.tag_hello.name, post_001_card.text)
+        self.assertNotIn(self.tag_ggmu.name, post_001_card.text)
+        self.assertNotIn(self.tag_love.name, post_001_card.text)
 
         post_002_card = main_area.find('div', id='post-2')
         self.assertIn(self.post_002.title, post_002_card.text)
         self.assertIn(self.post_002.category.name, post_002_card.text)
+        self.assertIn(self.post_002.author.username.upper(), post_002_card.text)
+        self.assertNotIn(self.tag_hello.name, post_002_card.text)
+        self.assertNotIn(self.tag_ggmu.name, post_002_card.text)
+        self.assertNotIn(self.tag_love.name, post_002_card.text)
 
         post_003_card = main_area.find('div', id='post-3')
         self.assertIn('미분류', post_003_card.text)
         self.assertIn(self.post_003.title, post_003_card.text)
+        self.assertIn(self.post_003.author.username.upper(), post_003_card.text)
+        self.assertNotIn(self.tag_hello.name, post_003_card.text)
+        self.assertIn(self.tag_ggmu.name, post_003_card.text)
+        self.assertIn(self.tag_love.name, post_003_card.text)
 
         # Post가 없는 경우
         Post.objects.all().delete()
@@ -117,6 +136,10 @@ class TestView(TestCase):
         self.assertIn(self.user_gomin0.username.upper(), post_area.text)
         # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(self.post_001.content, post_area.text)
+
+        self.assertIn(self.tag_hello.name, post_area.text)
+        self.assertNotIn(self.tag_ggmu.name, post_area.text)
+        self.assertNotIn(self.tag_love.name, post_area.text)
 
     def test_category_page(self):
         response = self.client.get(self.category_soccer.get_absolute_url())
