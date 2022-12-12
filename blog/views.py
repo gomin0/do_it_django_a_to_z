f35e512426115c790a3ef2352180e82d28bc0705
gomin0 <gomin0981113@naver.com>
2022-12-12 13:51:11 +0900

    pagination으로 포스트를 나누어 보여 주기

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,11 @@ from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 
+
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
